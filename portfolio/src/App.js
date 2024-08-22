d192ac8bc06d70d30da80318768b8c83b1ef900d
Hamzah Patel <hamzahpatel403@gmail.com>
2024-08-23 00:44:10 +1200

    Added Styling, navbar and button component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        test
-      </p>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact/>
+      </Routes>
+    </Router>
   );
 }
 
