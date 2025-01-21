7024dab8f4c9d5af40f330d85ac5cf3a9c3d1433
Hamzah Patel <110585134+realiru@users.noreply.github.com>
2025-01-21 21:15:22 +0800

    completed responsive hero section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar.js';
 import './App.css';
+import Home from './components/pages/Home.js';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' exact/>
+        <Route path='/' exact element={<Home />} />
       </Routes>
     </Router>
   );
--- portfolio/src/components/HeroSection.js
+++ portfolio/src/components/HeroSection.js
@@ -6,7 +6,7 @@ import '../App.css'
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-2.mp4' autoPlay loop muted />
+      {/* <video src='/videos/video-2.mp4' autoPlay loop muted /> */}
       <h1> ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
@@ -19,19 +19,12 @@ function HeroSection() {
         </Button>
         <Button
           className='btns'
-          buttonStyle='btn--outline'
+          buttonStyle='btn--primary'
           buttonSize='btn--large'
           >
-          GET STARTED
+          WATCH TRAILER <i className='far fa-play-circle' />
         </Button>
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
-          >
-          GET STARTED
-        </Button>
-      </div>
+        </div>
     </div>
   )
 }
