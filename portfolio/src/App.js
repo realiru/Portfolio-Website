4a62037c5887f9a3fdba0fa151244ff163d94083
Hamzah Patel <110585134+realiru@users.noreply.github.com>
2025-02-14 19:13:35 +0700

    Added extra pages
    Added 3 more placeholder pages with working navigation

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar.js';
 import './App.css';
 import Home from './components/pages/Home.js';
+import Services from './components/pages/Services.js';
+import SignUp from './components/pages/SignUp.js';
+import Products from './components/pages/Products.js';
+
 
 function App() {
   return (
@@ -9,6 +13,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' exact element={<Home />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/sign-up' element={<SignUp />} />
       </Routes>
     </Router>
   );
