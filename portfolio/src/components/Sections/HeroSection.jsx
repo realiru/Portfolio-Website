e757237063d7d2c38931debbe86a40e9d045437e
Hamzah Patel <hamzahpatel403@gmail.com>
2026-05-12 22:19:16 +1200

    Hero Section

--- portfolio/src/components/Sections/HeroSection.jsx
+++ portfolio/src/components/Sections/HeroSection.jsx
@@ -1,50 +1,11 @@
-import React, { useEffect, useRef } from 'react';
-import Typed from 'typed.js';
 import './HeroSection.css';
-import { Button } from '../Button';
-import '../../App.css';
 
 function HeroSection() {
-  const subTextElement = useRef(null);
-
-  useEffect(() => {
-    const subTextOptions = {
-      strings: [
-        'a Full Stack Developer',
-        'a Computer Enthusiast',
-        'a Problem Solver',
-        'a Cyber Security Consultant',
-        'a Fast Learner',
-      ],
-      typeSpeed: 50,
-      backSpeed: 50,
-      backDelay: 2000,
-      loop: true,
-    };
-
-    const subTyped = new Typed(subTextElement.current, subTextOptions);
-
-    return () => {
-      subTyped.destroy();
-    };
-  }, []);
-
   return (
-    <div className='hero-container'>
-      {/* <video src='/videos/video-2.mp4' autoPlay loop muted /> */}
-      <h1>Hi, I'm <p style={{ color: '#5D3A1A' }}>Hamzah</p></h1>
-      <p>
-        I am <span ref={subTextElement}></span>
-      </p>
-      <div className='hero-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-        >
-          View My Work
-        </Button>
-        </div>
+    <div className="hero-container">
+      <h1>Hi, I'm Hamzah</h1>
+      <p>Computer Science Graduate</p>
+      <a href="#about" className="hero-btn">View My Work</a>
     </div>
   );
 }
