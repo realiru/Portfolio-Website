3d9f59c8bbfa9cb4e819260fc940bc12955f1bbd
Hamzah Patel <hamzahpatel403@gmail.com>
2026-05-12 22:19:26 +1200

    Update Home.jsx

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,23 +1,9 @@
-import "../App.css";
-import React from "react";
-import HeroSection from "./Sections/HeroSection";
-import Cards from "./Sections/Project";
-import Footer from "./Sections/Footer";
-import AboutMe from "./Sections/AboutMe";
-import Skills from "./Sections/Skills";
-import Work from "./Sections/Work";
-import Education from "./Sections/Education";
+import HeroSection from './Sections/HeroSection.jsx';
 
 function Home() {
   return (
     <>
       <HeroSection />
-      <AboutMe />
-      <Skills />
-      <Work />
-      {/* <Education /> */}
-      <Cards />
-      <Footer />
     </>
   );
 }
